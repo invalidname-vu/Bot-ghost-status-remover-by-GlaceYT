37722bc56a15ce41f3e18f302f4ba9adeee47d82
invalidname-vu <68509631+invalidname-vu@users.noreply.github.com>
2025-02-18 03:10:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 Playing Genshin Impact", "Paimon, best travel companion ever!"];
+const statusMessages = ["🎮 Playing Genshin Impact", "Paimon, best travel companion ever!", "I am eating Hamburger", "and Potato", "everyday", "Potato is", "YAY", "Vegetables", "YAY", "eating eating eating eating", "EVERY NIGHT"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
